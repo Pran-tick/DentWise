6a14ab563eca4e16841a40b1563aa7ea486f2b18
Prantik Verma <prantikkvermaa4@gmail.com>
2025-11-04 19:11:35 +0530

    landing page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignUpButton,
-  SignedOut,
-  SignOutButton,
-  SignedIn,
-} from "@clerk/nextjs";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>home page</h1>
-      <SignedOut>
-        <SignUpButton mode="modal">sign up</SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton>Logout</SignOutButton>
-      </SignedIn>
+    <div className="min-h-screen bg-background">
+      <Header />
+      <Hero />
+      <HowItWorks />
+      <WhatToAsk />
+      <PricingSection />
+      {/* <CTA />
+      <Footer /> */}
     </div>
   );
 }
